f1da3b6c4aa23d762a9a0dd27d86e7d442b22665
Dog99 <quangsoi99@gmail.com>
2020-05-19 19:11:44 +0700

    Update Giao Dien

---
+++ QuanLyQuanCafe/DAO/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace QuanLyQuanCafe.DAO
+{
+   
+    public class DataProvider
+    {
+        private string connect = "Data Source=GL-522VJ\\SQLEXPRESS;Initial Catalog=QuanLyQuanCafe;Integrated Security=True";
+
+        public DataTable ExecuteQuery (string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connect))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+              
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+            }
+            return data;
+        }
+        
+    }
+}
